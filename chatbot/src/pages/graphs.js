fdc802a21d7e36906b2211f2369d7f0edf798223
Hashim Chaudhry <hashimc117@gmail.com>
2022-04-16 23:30:34 -0700

    Started working on graphs page

--- chatbot/src/pages/graphs.js
+++ chatbot/src/pages/graphs.js
@@ -1,8 +1,43 @@
 import React from 'react'
+import SideBar from "../components/sidebar"
+import styles from "../../src/css/globals.module.css"
+import TopBar from "../components/topbar";
+import { Chart } from "react-chartjs-2"
 
 const Graphs = () => {
+  const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
+
   return (
-    <h1>graphs</h1>
+    <div className={styles.page}>
+        <SideBar />
+        <div className={styles.content}>
+            <TopBar/>
+            <Chart type="line" options={} data={data}/>
+        </div>
+    </div>
   )
 }
 
